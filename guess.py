01d0733b86ef996856fdf367e508231453a7eefb
kinjalsri <kinjalsri01@gmail.com>
2024-08-23 12:21:24 +0530

    GAME WORKING

--- guess.py
+++ guess.py
@@ -1,14 +1,43 @@
-words= ["camel" , "love", "hate"]
 import random
+from hangwords import words
+from hangmanlives import logo, stages
 
-chosen= random.choice(words)
-print(chosen)
+print(logo)
 
-guess= input("guess a letter").lower()
-print(guess)
+chosen = random.choice(words)
 
-for letter in chosen:
-    if letter==guess:
-        print("right")
-    else:
-        print("wrong")
+
+placeholder = "_ " * len(chosen)
+print(placeholder)
+
+game_over = False 
+correctletters = []
+lives = 6
+
+while not game_over:
+
+    print(f"you have {lives} lives left")
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+    for letter in chosen:
+        if letter == guess or letter in correctletters:
+            display += letter
+            correctletters.append(letter)
+        else:
+            display += "_"
+    
+    print(display)
+
+    if guess not in chosen:
+        lives -= 1
+        print(stages[lives])
+        if lives == 0:
+            game_over = True
+            print(f"The word was {chosen}\n")
+                  
+            print("     ***You lose***    ")
+
+    if "_" not in display:
+        game_over = True
+        print("     ***You win***     ")
